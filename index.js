b1e2c39f58bf94ce9534b353e302e78765f7b970
ClintFix <clint.fix@gmail.com>
2021-01-27 13:26:33 -0700

    task 3 complete

--- index.js
+++ index.js
@@ -113,10 +113,17 @@ personOne.toString();
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
- 
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age); //inheriting Person attributes
+   this.favoriteToy = favoriteToy;
+ }
+ Baby.prototype = Object.create(Person.prototype);
+ Baby.prototype.play = function(){
+   return `Playing with ${this.favoriteToy}`;
+ }
+
+ const newBaby = new Baby('Rylee','2','dresses'); 
+ console.log(newBaby.play());
   
   /* 
     TASK 4
